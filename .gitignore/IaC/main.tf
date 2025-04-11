20dcaed0813c53f49967699fffc926feef12f2f6
Simeon Petkov <simeon_kill@abv.bg>
2025-04-11 08:14:24 +0000

    Fixed the ALB target groups and listeners (Frontend and Backend)

--- .gitignore/IaC/main.tf
+++ .gitignore/IaC/main.tf
@@ -334,9 +334,9 @@ resource "aws_security_group" "lb_security_group" {
 }
 
 
-##########################################################################
+########################################################################################################
 # Target Groups Creation for ALB (Frontend / Backend)
-##########################################################################
+########################################################################################################
 
 
 # Frontend Target Group Creation: (httpd service)
@@ -384,14 +384,9 @@ resource "aws_lb_target_group" "backend_tg" {
 
 
 
-
-
-
-
-
-########################################################################################################
+##################################################################################################################
 # Application Load Balancer (ALB) / ALB Listeners for HTTP and HTTPS Routing To The ALB Target Group 
-########################################################################################################
+##################################################################################################################
 
 # Define the Application Load Balancer
 resource "aws_lb" "web_alb" {
@@ -417,7 +412,7 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg.arn
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
 
     redirect {
       protocol    = "HTTPS"
@@ -429,6 +424,7 @@ resource "aws_lb_listener" "http" {
 }
 
 # Define ALB HTTPS Listener (With the relevant TLS Cert:)
+# Forward if path is `/` or `/*.html` or similar → frontend-tg
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 443
@@ -443,6 +439,7 @@ resource "aws_lb_listener" "https" {
 }
 
 # Listener Rule To Send /api/* To Backend:
+# Forward if path starts with `/api/*` → backend-tg
 resource "aws_lb_listener_rule" "backend_api_route" {
   listener_arn = aws_lb_listener.https.arn
 
@@ -552,8 +549,11 @@ resource "aws_autoscaling_group" "web_server_asg" {
   desired_capacity     = 1
   vpc_zone_identifier  = [aws_subnet.public_subnet.id, aws_subnet.public_subnet_2.id]  # Deploy EC2s in both subnets
 
-  # Correct EC2 target group ARN here
-  target_group_arns    = [aws_lb_target_group.web_tg.arn]
+  # Correct ALB Target Groups ARN here:
+  target_group_arns = [
+  aws_lb_target_group.frontend_tg.arn,
+  aws_lb_target_group.backend_tg.arn
+  ]
 
   tag {
     key                 = "Name"
