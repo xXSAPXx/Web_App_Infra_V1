90d5fd15e0b4ab48dd564f3191ffe8b098083f7b
Simeon Petkov <simeon_kill@abv.bg>
2025-05-06 10:40:33 +0000

    added VPC hostnames enabled

--- .gitignore/IaC/main.tf
+++ .gitignore/IaC/main.tf
@@ -64,6 +64,8 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name = "App_VPC_IaC"
   }
