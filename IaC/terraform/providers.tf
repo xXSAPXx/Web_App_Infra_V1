98c76ab0f18654de6070088baa357e566d4812f7
xXSAPXx <simeon_kill@abv.bg>
2025-06-12 22:11:57 +0300

    Added new providers.tf file

---
+++ IaC/terraform/providers.tf
@@ -0,0 +1,16 @@
+
+# Required Providers: 
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
+ }
+}
